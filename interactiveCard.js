d396f32a0dc3afadb4d181eda9e32c6eed4f6bfe
Teetytweet <55676490+Teetytweet@users.noreply.github.com>
2019-09-29 12:41:56 -0700

    Update interactiveCard.js

--- interactiveCard.js
+++ interactiveCard.js
@@ -1,1 +1,14 @@
+
 function draw (){
+  fill(255,0,0);
+ellipse (200,200,50,50);
+  if(keyDown("g"){
+     ellipse.x+=1;
+     ellipse.y+=1;
+     }
+     if(keyDown("s"){
+    ellipse.x-=1;
+    ellipse.y-=1;
+  }
+  drawsprites()
+}
